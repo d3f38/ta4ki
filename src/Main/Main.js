5a49907c21f09b67ae0546b5e09924d21e167f3b
Aleksandr Bylkov <abylkov.kameleoon.com>
2019-12-25 20:03:52 +0300

    add sort by price and distance

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,41 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import cars from '../cars'
 
 import Card from  './../Card/Card';
 import './../Card/Card.scss';
+import './Main.scss';
 
 const haversine = require('haversine');
 
-function Main() {
+class Main extends Component {
+    state = {
+        data: cars,
+        sorting: 'price',
+        initLocation: {'latitude': 55.7536232, 'longitude': 37.6199775}
+    }
+
+    componentDidMount() {
+        this.sortDealersByPrice(this.state.data);
+    }
+
     
-    const [data, setState] = useState(cars);
-    const [sorting, setSorting] = useState('price');
-    const [initLocation, setInitLocation] = useState({"latitude": 55.7536232,
 
-    "longitude": 37.6199775});
+    sortDealersByDistance = ({ data, initLocation }) => {
+        const calculateDistance = (start, end) => Math.round(haversine(start, end));
 
-    console.log('data', data)
-    useEffect(() => {
-        document.title = `Вы нажали ${data} раз`;
-    });
-
-    const start = {
-        latitude: 30.849635,
-        longitude: -83.24559
-    }
-      
-    const end = {
-        latitude: 27.950575,
-        longitude: -82.457178
-    }
-      
-    const calculateDistance = (start, end) => Math.round(haversine(start, end));
- 
-    const sortPrice = (data) => {
-        // console.log(data.forEach(item => {
-
-        // }))
-        return data.sort((a,b) => {
+        const sortedData = data.sort((a,b) => {
             const aLocation = { 
                 latitude: a.dealer.latitude,
                 longitude: a.dealer.longitude
@@ -46,46 +34,71 @@ function Main() {
                 longitude: b.dealer.longitude
             }
 
-            const aDistance = calculateDistance(initLocation, aLocation) || 0;
-            const bDistance = calculateDistance(initLocation, bLocation) || 0;
+            const aDistance = calculateDistance(initLocation, aLocation);
+            const bDistance = calculateDistance(initLocation, bLocation);
 
-            console.log(aDistance, bDistance)
-            if (aDistance > bDistance) {
+            if (aDistance > bDistance || isNaN(aDistance)) {
                 return 1;
-              }
-              if (aDistance < bDistance) {
+            }
+            if (aDistance < bDistance || isNaN(bDistance)) {
                 return -1;
-              }
-              // a должно быть равным b
-              return 0;
+            }
+            return 0;
         });
+
+        this.setState({
+            data: sortedData,
+            sorting: 'distance'
+        })
     }
 
-    console.log(sortPrice(data))
-    return (
-		<div className="Main">
-            <div className='Sorting'>
-                <span className='Sorting-Title'></span>
-                <div className='Sorting-Price'>
-                    <input type='radio' className='Sorting-Input' onChange={sortPrice} id='sorting-price' name='sorting'  hidden></input>
-                    <label className='Sorting-Label'htmlFor='sorting-price'>
-                        <div className='Sorting-Radio'></div>
-                        <span className='Sorting-Type'>По цене</span>
-                    </label>
+    handlerSortDealersByDistance = () => this.sortDealersByDistance(this.state);
+
+    sortDealersByPrice = () => {
+        const sortedData = this.state.data.sort((a,b) => {
+            if (parseInt(a.price) > parseInt(b.price)) {
+                return 1;
+            }
+            if (parseInt(a.price) < parseInt(b.price)) {
+                return -1;
+            }
+            return 0;
+        });
+
+        this.setState({
+            data: sortedData,
+            sorting: 'price'
+        })
+    }
+
+    render () {
+        console.log(this.state)
+
+        return (
+            <div className="Main">
+                <div className='Sorting'>
+                    <span className='Sorting-Title'></span>
+                    <div className='Sorting-Price'>
+                        <input type='radio' className='Sorting-Input' onChange={this.sortDealersByPrice} id='sorting-price' name='sorting'  hidden></input>
+                        <label className='Sorting-Label'htmlFor='sorting-price'>
+                            <div className='Sorting-Radio'></div>
+                            <span className='Sorting-Type'>По цене</span>
+                        </label>
+                    </div>
+                    <div className='Sorting-Distance'>
+                        <input type='radio' className='Sorting-Input' onChange={this.handlerSortDealersByDistance} id='sorting-distance' name='sorting' hidden></input>
+                        <label className='Sorting-Label' htmlFor='sorting-distance'>
+                            <div className='Sorting-Radio'></div>
+                            <span className='Sorting-Type'>По удаленности</span>
+                        </label>
+                    </div>
                 </div>
-                <div className='Sorting-Distance'>
-                    <input type='radio' className='Sorting-Input' onChange={sortPrice} id='sorting-distance' name='sorting' hidden></input>
-                    <label className='Sorting-Label' htmlFor='sorting-distance'>
-                        <div className='Sorting-Radio'></div>
-                        <span className='Sorting-Type'>По удаленности</span>
-                    </label>
+                <div className='Catalogue'>
+                    {this.state.data.map(item => Card(item))}
                 </div>
             </div>
-            <div className='Catalogue'>
-                {data.map(item => Card(item))}
-            </div>
-        </div>
-	);
+        );
+    }
 }
 
 export default Main;
